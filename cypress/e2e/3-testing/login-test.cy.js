63bee071f52dce38e3f759a27c3fe68105751812
Tokle89 <fredriktokle@gmail.com>
2023-10-27 16:53:10 +0200

    creates login test

--- cypress/e2e/3-testing/login-test.cy.js
+++ cypress/e2e/3-testing/login-test.cy.js
@@ -1,21 +1,36 @@
-describe("login error  test", () => {
-  it("get error message when logging in", () => {
-    cy.visit("http://localhost:5500");
+// describe("login error  test", () => {
+//   it("get error message when logging in", () => {
+//     cy.visit("http://localhost:5500");
 
-    cy.get("#email").type("toklus234@gmail.com");
-    // cy.get("#mail").should("have.value", "toklus234@gmail.com");
+//     cy.get("#email").type("toklus234@gmail.com");
+//     // cy.get("#mail").should("have.value", "toklus234@gmail.com");
 
-    cy.get("#Password").type("password123");
-    // cy.get("#Password").should("have.value", "password123");
+//     cy.get("#Password").type("password123");
+//     // cy.get("#Password").should("have.value", "password123");
 
-    cy.get("#login-btn").click();
+//     cy.get("#login-btn").click();
 
-    cy.get(".login-msg").contains("Wrong username or password");
-  });
-});
+//     cy.get(".login-msg").contains("Wrong username or password");
+//   });
+// });
 
 // describe("The Home Page", () => {
 //   it("successfully loads", () => {
 //     cy.visit("http://localhost:5500");
 //   });
 // });
+
+describe("login test", () => {
+  it("logs in, and takes user to profile page", () => {
+    cy.visit("http://localhost:5500");
+
+    cy.get("#email").type("tokle@noroff.no");
+    cy.get("#Password").type("fingern234");
+
+    cy.get("#login-btn").click();
+
+    cy.wait(2000);
+
+    cy.url().should("include", "/profile");
+  });
+});
